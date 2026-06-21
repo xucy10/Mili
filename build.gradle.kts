006a0ef7a16f4d67b25cce912d7c6e4da809f5be
xucy <someonm201141923@outlook.com>
2026-06-21 10:23:20 +0800

    fix(build): add Maven publications for repository upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -87,6 +87,14 @@ subprojects {
     }
 
     extensions.configure<PublishingExtension> {
+        publications {
+            create<MavenPublication>("mili") {
+                groupId = project.group.toString()
+                artifactId = project.name
+                version = project.version.toString()
+                from(components["java"])
+            }
+        }
         repositories {
             maven("https://repo.menthamc.org/repository/maven-snapshots/") {
                 name = "MenthaMC"
